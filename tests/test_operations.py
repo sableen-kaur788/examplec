c71793016735a64c55e89f692f7746680c6c6650
Sableen Kaur <your-email@example.com>
2026-02-03 15:55:51 +0530

    adding yml file

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -2,7 +2,7 @@ from src.math_operations import add, sub
 
 def test_add():
     assert add(2,3) == 5
-    assert add(1,3) == 3
+    assert add(1,3) == 4
     assert add(-1,1) == 0
     
 def test_sub():
